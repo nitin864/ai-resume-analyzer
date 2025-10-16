88a6f50bec34013bc87c16158c858155e6bbae1c
Nitin <rajnitin793@gmail.com>
2025-10-16 08:35:19 +0530

    updated styling and add some annimation to card

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router'
 
-const ResumeCard = () => {
+const ResumeCard = ({resume} : {resume: Resume}) => {
   return ( 
-    <div>ResumeCard</div>
+    <Link to={`/resume/${resume.id}`} className='resume-card animate-in fade-in duration-1000'>Resume Card</Link>
   )
 }
 
