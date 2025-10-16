5d6fc5d1a0e2f9ae67a1e23970a83e7b0c787d2e
Nitin <rajnitin793@gmail.com>
2025-10-16 08:13:02 +0530

    Enhanced some UI Part and added dummy data, a resume card on 15th oct 2025 8:12 A:M

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,37 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants";
+
+import { resume } from "react-dom/server";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResumeIQ" },
+    { name: "description", content: "Smarter insights for smarter careers." },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <>
+      <main className="bg-[url('/images/bg-main.svg')]">
+        <Navbar />
+        <section className="main-section">
+          <div className="page-heading">
+            <h1>Track Your Application and Resume Ratings</h1>
+            <h2>Optimize your job applications with AI insights.</h2>
+          </div>
+        </section>
+
+        {resume.length > 0 && (
+          <div className="resume-section">
+            {resumes.map((resume: any) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </main>
+    </>
+  );
 }
