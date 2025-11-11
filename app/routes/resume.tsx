83746dbcfe07537f728f7435c565d65d268f0eeb
Nitin <rajnitin793@gmail.com>
2025-11-11 21:56:06 +0530

    added some ui in resume page

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,12 +1,39 @@
-import React from 'react'
-import { useParams } from 'react-router'
+import { Link, useParams  } from 'react-router'
+import React, { useState, useEffect } from "react";
+
+export const meta = () => [
+  { title: "Resume IQ | Review" },
+  { name: "description", content: "Detailed Overview of Your Resume" },
+];
 
 const resume = () => {
 
   const {id}  = useParams();
+  
+  const useEffect(() => {
+        const
+  } , [id])
 
   return (
-    <div>resume {id}</div>
+     <main className='!pt-0'>
+        <nav className='resume-nav'>
+            <Link to="/" className='back-button'>
+              <img src='/icons/back.svg' alt='logo'  className='w-2.5 h-2.5'/>
+              <span className='text-gray-800 text-sm font-semibold'>Back to Home</span>
+            </Link>
+            <div className='flex flex-row w-full max-lg:flex-col'>
+              <section className='feedback-section '>
+                {imageUrl && resumeUrl && (
+                   
+                   <div className='animate-in fade-in duration-1000 gradient-border max-smm-0 h-[90%] max-wxl:h-fit w-fit'>
+
+                   </div>
+
+                )}
+              </section>
+            </div>
+        </nav>
+     </main>
   )
 }
 
