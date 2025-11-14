ed0e2cae598ef3b63aa2ed9fccc7368f75c68584
Nitin <rajnitin793@gmail.com>
2025-11-14 13:04:01 +0530

    update till 1:03 pm

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+ 
+import ScoreGauge from './ScoreGauge'
 
-const Summary = () => {
+const Summary = ({ feedback}: {feedback: Feedback}) => {
   return (
     <div className='rouded-2xl bg-white shadow-md w-full'>
-        <div>
-            
+        <div className='flex flex-row p-4  items-center gap-8 '>
+             <ScoreGauge score = {feedback.overallScore} />
+             <div className='flex flex-col gap-2'>
+                <h2 className='text-2xl font-bold'>Your Resume Score</h2>
+                <p className='text-sm  text-gray-500'> 
+                    These Scores Calculated Based on the variables listed Below..
+                </p> 
+             </div>
         </div>
+        
     </div>
   )
 }
